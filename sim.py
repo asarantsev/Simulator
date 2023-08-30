4e931cd07cb6799bf78c7d05aa062969b2cf9231
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2023-08-30 10:35:28 -0700

    Update sim.py

--- sim.py
+++ sim.py
@@ -4,17 +4,11 @@ from matplotlib import pyplot
 
 def MC(m, s, N):
   sims = numpy.random.normal(m, s, N)
-  pyplot.hist(sims, bins = int(numpy.sqrt(N)))
-  pyplot.show()
+  fig = pyplot.hist(sims, bins = int(numpy.sqrt(N)))
+  streamlit.pyplot(fig)
   return numpy.mean(sims)
 
-def main():
-  m = input('Mean')
-  s = input('Stdev')
-  N = input('Sample')
-  print('empirical mean = ', MC(m, s, N))
-  return 0
-
-if __name__ == "__main__":
-    main()
-
+m = streamlit.number_input('mean') 
+s = streamlit.number_input('stdev', min_value = 0)
+N = streamlit.number_input('size', min_value = 1, step = 1)
+print('empirical mean = ', MC(m, s, N))
