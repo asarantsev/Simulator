1001832488dfd6ba94b940f1ee6cedaf2e774128
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2023-08-29 20:32:53 -0700

    Create sim.py

---
+++ sim.py
@@ -0,0 +1,19 @@
+import numpy
+from matplotlib import pyplot
+
+def MC(m, s, N):
+  sims = numpy.random.normal(m, s, N)
+  pyplot.hist(sims, bins = int(numpy.sqrt(N)))
+  pyplot.show()
+  return numpy.mean(sims)
+
+def main():
+  m = input('Mean')
+  s = input('Stdev')
+  N = input('Sample')
+  print('empirical mean = ', MC(m, s, N))
+  return 0
+
+if __name__ == "__main__":
+    main()
+
